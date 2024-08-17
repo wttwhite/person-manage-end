0ac225947fe47e5467e4f76a2f7ba6ac1cce6709
wttwhite <wangtt12034@126.com>
2024-08-17 15:41:47 +0800

    feat: init

---
+++ src/main/java/com/test/usercanter/UserCanterApplication.java
@@ -0,0 +1,13 @@
+package com.test.usercanter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCanterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCanterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/usercanter/UserCanterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.usercanter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCanterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
