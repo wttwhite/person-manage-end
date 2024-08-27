5dc7023088af5eecc932bc0d4e0334156d5778d9
wttwhite <wangtt12034@126.com>
2024-08-27 16:04:25 +0800

    test报错解决--->删除一开始建项目初始的mybatis依赖

--- src/test/java/com/test/usercanter/UserCanterApplicationTests.java
+++ src/test/java/com/test/usercanter/UserCanterApplicationTests.java
@@ -5,7 +5,7 @@ import com.test.usercanter.model.User;
 import org.junit.Test;
 import org.junit.Assert;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -24,7 +24,7 @@ public class UserCanterApplicationTests {
     public void testSelect() {
         System.out.println(("----- selectAll method test ------"));
         List<User> userList = userMapper.selectList(null);
-        Assert.assertEquals(5 ,userList.size());
+        Assert.assertEquals(1 ,userList.size());
         userList.forEach(System.out::println);
     }
 
